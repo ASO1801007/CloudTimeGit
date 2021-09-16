58be6361d83d133e0bbfa8410609c9fc42b69526
hikal <1801007@s.asojuku.ac.jp>
2021-09-16 10:52:42 +0900

    モデル作成_江崎

---
+++ CloudTimeApp/app/Models/User_info.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User_info extends Model{
+
+    protected $guarded = ['id'];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+}
