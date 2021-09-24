25640de8be44108c02500456d2baeb3b28d93d7d
hikal <1801007@s.asojuku.ac.jp>
2021-09-24 10:21:52 +0900

    ログイン時トップにいく江崎

--- CloudTimeApp/app/Http/Controllers/HomeController.php
+++ CloudTimeApp/app/Http/Controllers/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Member;
+use App\Models\Capsule;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +28,22 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //ログインしているユーザーのIDを取得
+        $login = Auth::id();
+
+        //ログインしているユーザーIDを取得
+        $user_id = User::find($login)->id;
+        
+        //自分が参加しているカプセル情報を取得
+        $join_capsule_id = Member::where('user_id',$login)->get();
+        $count = count($join_capsule_id);
+        
+        //変数のままじゃ送れないため、代入
+        $data = ["capsule_data"=>$join_capsule_id, "count"=>$count];
+
+        //return
+        return view('Top.Top', $data);
+
+        // return view('Top.top');
     }
 }
