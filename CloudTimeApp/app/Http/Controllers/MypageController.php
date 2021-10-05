57c00842e9e960946c044b01b256e7047bb8044c
aso1801016 <1801016@s.asojuku.ac.jp>
2021-10-05 14:10:57 +0900

    鍬崎

--- CloudTimeApp/app/Http/Controllers/MypageController.php
+++ CloudTimeApp/app/Http/Controllers/MypageController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MypageController extends Controller
-{
-    //
+class MypageController extends Controller{
+    public function mypage($id=0){
+        return view('mypage');
+    }
 }
---
+++ CloudTimeApp/resources/views/mypageedit.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>マイページ</title>
+</head>
+<body>
+<form action="#" method="post">
+	<p>ユーザー名：<br>
+	<input type="text" name="name"></p>
+
+    <form action="#" method="post">
+	<p>生年月日：<br>
+	<input type="text" name="day"></p>
+
+    <form action="#" method="post">
+	<p>所在地:<br>
+	<input type="text" name="place"></p>
+
+    <form action="#" method="post">
+	<p>職業:<br>
+	<input type="text" name="job"></p>
+
+    <form action="#" method="post">
+	<p>出身高校:<br>
+	<input type="text" name="hschool"></p>
+
+    <form action="#" method="post">
+	<p>出身中学校:<br>
+	<input type="text" name="jhschool"></p>
+
+	<form action="#" method="post">
+	<p>出身小学校:<br>
+	<input type="text" name="jschool"></p>
+
+	<button type="button">更新</button>
+
+</form>
+</form>
+</body>
+</html>
