fd781085a8c7d1b707544a6485f6b16d1f5f178f
ASO1801023 <1801023@s.asojuku.ac.jp>
2021-10-14 09:50:05 +0900

    ログイン画面に戻す

--- CloudTimeApp/routes/web.php
+++ CloudTimeApp/routes/web.php
@@ -22,33 +22,33 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //トップ画面
-Route::get('/top', [App\Http\Controllers\TopController::class, 'top'])->name('top.top');
+Route::get('/top', [App\Http\Controllers\TopController::class, 'top'])->name('top.top')->middleware('auth');
 
 //チャット画面
-Route::get('/chat', [App\Http\Controllers\ChatController::class,'chat']);
+Route::get('/chat', [App\Http\Controllers\ChatController::class,'chat'])->middleware('auth');
 
 //エントリー画面
-Route::get('/entry', [App\Http\Controllers\EntryController::class,'entry']);
+Route::get('/entry', [App\Http\Controllers\EntryController::class,'entry'])->middleware('auth');
 
 //マイページ画面
-Route::get('/mypage_info/{user_id}', [App\Http\Controllers\MypageController::class,'show_info'])->name('mypage.show_info');
+Route::get('/mypage_info/{user_id}', [App\Http\Controllers\MypageController::class,'show_info'])->name('mypage.show_info')->middleware('auth');
 //マイページ編集
-Route::get('/mypage_edit', [App\Http\Controllers\MypageController::class,'show_edit']);
+Route::get('/mypage_edit', [App\Http\Controllers\MypageController::class,'show_edit'])->middleware('auth');
 //マイページ更新->マイページ画面
-Route::post('/mypage_update', [App\Http\Controllers\MypageController::class,'mypage_update']);
+Route::post('/mypage_update', [App\Http\Controllers\MypageController::class,'mypage_update'])->middleware('auth');
 
 //カプセル作成画面
-Route::get('/capsule_entry',[App\Http\Controllers\CapsuleController::class,'show_entry']);
+Route::get('/capsule_entry',[App\Http\Controllers\CapsuleController::class,'show_entry'])->middleware('auth');
 
 //カプセル作成->ホーム画面
-Route::post('/capsule_create',[App\Http\Controllers\CapsuleController::class,'capsule_create']);
+Route::post('/capsule_create',[App\Http\Controllers\CapsuleController::class,'capsule_create'])->middleware('auth');
 
 //カプセル詳細画面
-Route::get('/capsule_info/{capsule_id}',[App\Http\Controllers\CapsuleController::class,'show_info']);
+Route::get('/capsule_info/{capsule_id}',[App\Http\Controllers\CapsuleController::class,'show_info'])->middleware('auth');
 
 //カプセル削除->ホーム画面
-Route::post('/capsule_delete',[App\Http\Controllers\CapsuleController::class,'capsule_delete']);
+Route::post('/capsule_delete',[App\Http\Controllers\CapsuleController::class,'capsule_delete'])->middleware('auth');
 
 //画像追加画面
-Route::post('/capsule_info/image',[App\Http\Controllers\ImageController::class,'index'])->name('image.index');
-Route::post('/capsule_info//image',[App\Http\Controllers\ImageController::class,'store'])->name('image.store');
+Route::post('/capsule_info/image',[App\Http\Controllers\ImageController::class,'index'])->name('image.index')->middleware('auth');
+Route::post('/capsule_info//image',[App\Http\Controllers\ImageController::class,'store'])->name('image.store')->middleware('auth');
